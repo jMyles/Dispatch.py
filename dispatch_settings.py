6f0b6b53d2410343b88c376ffaa97771884fc0ad
KPrasch <kieranprasch@gmail.com>
2015-02-13 22:41:14 -0800

    Created borader project file structure

--- dispatch_settings.py
+++ dispatch_settings.py
@@ -5,18 +5,6 @@ LOCALE_ZIP
 LOCALE_NOAA_STATION_CALL_SIGN 
 
 DELINIATER = ':'
-INCIDENT_FIELDS = (
-                   'Unit', \
-                   'Venue', \
-                   'Inc', \
-                   'Nature', \
-                   'XSts', \
-                   'Common', \
-                   'Addtl', \
-                   'Loc', \
-                   'Date', \
-                   'Time', \
-                   )
 
 LOCATION_FIELDS = ['XSts', 'Loc', 'Venue'] 
 
